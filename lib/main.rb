7c1e3d1a2a84d58d7f9ac2c3949106e1bc0f59d8
Roman Geathers <romangeathersdev@gmail.com>
2022-06-22 18:52:31 -0400

    Add methods to display guesses left and word state
    Add new methods to display the number of wrong guesses a player has
    left, and the current state of how far they've come in solving the
    secret word.

--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,11 @@
 class Game
+  attr_accessor :incorrect_guesses, :word_display
   attr_reader :random_word
 
   def initialize
     @random_word = ""
-    @incorrect_guess = 6
+    @incorrect_guesses = 6
+    @word_display = []
   end
 
   def get_random_word
@@ -11,10 +13,29 @@ class Game
       @random_word = File.readlines("google-10000-english-no-swears.txt").map(&:chomp).sample
     end
   end
+
+  def show_wrong_guesses_left
+    puts "You have #{@incorrect_guesses} wrong guess(es) left."
+  end
+
+  def display_word_state
+    for i in 0..(self.random_word.length - 1)
+      @word_display << "__"
+    end
+    
+    @word_display.each do |char|
+      print "#{char} "
+    end
+    puts "\n"
+  end
 end
 
 
 game = Game.new
-secret_word = game.get_random_word
+game.get_random_word
 
+game.show_wrong_guesses_left
 
+puts game.random_word
+
+game.display_word_state
