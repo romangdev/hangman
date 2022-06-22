78bd27493152db9ba0961a9c536f69b1a333ebc0
Roman Geathers <romangeathersdev@gmail.com>
2022-06-22 19:29:32 -0400

    Add methods to fill words with correct letters
    Add method for correct letter guesses that fills and displays the
    letters of the secret word the match the correct letter guess.
    
    Additionally, seperate and create individual methods for filling
    an empty word state (no guesses yet) and displaying the current word
    state. Previous combining of the 2 was causing bug.

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,5 @@
 class Game
+  attr_accessor :word_display
   attr_reader :random_word
 
   def initialize
@@ -17,26 +18,43 @@ class Game
     puts "You have #{@incorrect_guesses} wrong guess(es) left."
   end
 
-  def display_word_state
+  def fill_empty_word_state
     for i in 0..(self.random_word.length - 1)
       @word_display << "__"
     end
-    
+  end
+
+  def display_word_state
     @word_display.each do |char|
       print "#{char} "
     end
     puts "\n"
   end
+
+  def fill_guess(letter_guess)
+    if self.random_word.include?(letter_guess)
+      self.random_word.split("").each_with_index do |char, idx|
+        if char == letter_guess
+          self.word_display[idx] = letter_guess
+        else
+          self.word_display
+        end
+      end
+    else
+      false
+    end
+  end
 end
 
 class Player
+  attr_reader :letter_guess
   def initialize
     @letter_guess = ""
   end
 
   def get_letter_guess 
     puts "Guess a letter:"
-    @letter_guess = gets.chomp
+    @letter_guess = gets.chomp.downcase
     puts @letter_guess
   end
 end
@@ -51,6 +69,11 @@ game.show_wrong_guesses_left
 
 puts game.random_word
 
+game.fill_empty_word_state
 game.display_word_state
 
 player.get_letter_guess
+
+game.fill_guess(player.letter_guess)
+
+game.display_word_state
